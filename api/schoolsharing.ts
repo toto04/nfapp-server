5bde9d67c8a13fe37e2d967baefdadb3316a8de7
Tommaso Morganti <tommaso.morganti01@gmail.com>
2020-02-17 00:19:08 +0100

    note feedback

--- api/schoolsharing.ts
+++ api/schoolsharing.ts
@@ -25,10 +25,29 @@ ss.get('/note/:id', aw(async (req) => {
 
     let client = await pool.connect()
     let note = await client.query({
-        text: 'SELECT id, firstname || \' \' || lastname AS author, title, description, postingdate, data AS images FROM notes JOIN users ON "user" = users.username WHERE id = $1',
-        values: [req.params.id]
+        text:
+            `SELECT id,
+                data AS images,
+                firstname || ' ' || lastname AS author,
+                title,
+                description,
+                postingdate,
+                SUM(CASE
+                    WHEN notevotes.positive THEN 1
+                    WHEN notevotes.positive=false THEN -1
+                    ELSE 0 
+                END) as points,
+                n.positive as vote 
+            FROM notes 
+                JOIN users ON "user" = users.username 
+                LEFT JOIN notevotes ON notes.id = notevotes.note 
+                LEFT JOIN notevotes AS n ON notes.id=n.note AND n.user=$1 
+            WHERE id=$2 
+            GROUP BY notes.id, users.username, n.positive`,
+        values: [req.header('x-nfapp-username'), req.params.id]
     })
     client.release()
+    console.log(Object.keys(note.rows[0]).map(k => [k, typeof k]))
     return { success: true, data: note.rows[0] }
 }))
 
@@ -39,8 +58,26 @@ ss.route('/notes/:section/:class/:subject/:page*?')
 
         let client = await pool.connect()
         let notes = await client.query({
-            text: 'SELECT id, array_length(data, 1) AS images, firstname || \' \' || lastname AS author, title, description, postingdate FROM notes JOIN users ON "user" = users.username WHERE section = $1 AND notes.class = $2 AND subject = $3 ORDER BY postingdate LIMIT 10 OFFSET $4',
-            values: [req.params.section, req.params.class, req.params.subject, (parseInt(req.params.page ?? '0')) * 10]
+            text: `SELECT id,
+                array_length(data, 1) AS images,
+                firstname || ' ' || lastname AS author,
+                title,
+                description,
+                postingdate,
+                SUM(CASE
+                    WHEN notevotes.positive THEN 1
+                    WHEN notevotes.positive=false THEN -1
+                    ELSE 0 
+                END) as points,
+                n.positive as vote
+            FROM notes 
+                JOIN users ON "user" = users.username 
+                LEFT JOIN notevotes ON notes.id = notevotes.note 
+                LEFT JOIN notevotes AS n ON notes.id=n.note AND n.user=$5
+            WHERE section = $1 AND notes.class = $2 AND subject = $3
+            GROUP BY notes.id, users.username, n.positive
+            ORDER BY points DESC LIMIT 10 OFFSET $4`,
+            values: [req.params.section, req.params.class, req.params.subject, (parseInt(req.params.page ?? '0')) * 10, req.header('x-nfapp-username')]
         })
         client.release()
 
@@ -68,6 +105,23 @@ ss.route('/notes/:section/:class/:subject/:page*?')
             client.release()
         }
     }))
+
+ss.post('/vote/:note', aw(async (req) => {
+    const logged = await login(req.header('x-nfapp-username'), req.header('x-nfapp-password'))
+    if (!logged) return { success: false, error: 'invalid credentials' }
+
+    let client = await pool.connect()
+    if (req.body.vote == undefined) await client.query({
+        text: 'DELETE FROM notevotes WHERE "user"=$1 AND note=$2',
+        values: [req.header('x-nfapp-username'), req.params.note]
+    })
+    else await client.query({
+        text: 'INSERT INTO notevotes VALUES ($1, $2, $3) ON CONFLICT ON CONSTRAINT notevotes_pkey DO UPDATE SET positive=$3',
+        values: [req.header('x-nfapp-username'), req.params.note, req.body.vote]
+    })
+    client.release()
+    return { success: true }
+}))
 
 ss.get('/notes/:user', aw(async (req) => {
     const logged = await login(req.header('x-nfapp-username'), req.header('x-nfapp-password'))
